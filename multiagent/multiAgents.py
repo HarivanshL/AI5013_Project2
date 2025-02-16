93c4f1e8d49e417494dd8571c377f2ea962f338d
Dohyeong Park <bagdohyeong0@gmail.com>
2025-02-16 01:02:01 -0600

    Add exepectimax agent

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -279,7 +279,52 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        def expectimax(gameState: GameState, depth, agentIndex):
+            # if depth reached to max depth or game is end return the score
+            if depth == self.depth or gameState.isWin() or gameState.isLose():
+                return self.evaluationFunction(gameState)
+            
+            # Pacman's turn
+            if agentIndex == 0:
+                value = -float("inf")
+                best_action = None
+                # search all possible actions
+                for action in gameState.getLegalActions(agentIndex):
+                    successor = gameState.generateSuccessor(agentIndex, action)
+                    next_value = expectimax(successor, depth, agentIndex+1) # move to ghosts' turn
+                    if next_value > value:
+                        value = next_value
+                        best_action = action
+
+                # if depth is 0 return the best action
+                if depth == 0:
+                    return best_action if best_action is not None else Directions.STOP
+
+                return value
+            
+            # Ghosts' turn
+            else:
+                value = float("inf")
+                next_agent = agentIndex + 1
+
+                # if it is the last ghost, next will be pacman's turn. increase the depth
+                if next_agent == gameState.getNumAgents():
+                    next_agent = 0
+                    depth += 1
+
+                total = 0 # total value at next actions
+                action_count = 0 # the num of actions
+                
+                # search all possible actions
+                for action in gameState.getLegalActions(agentIndex):
+                    successor = gameState.generateSuccessor(agentIndex, action)
+                    total += expectimax(successor, depth, next_agent)
+                    action_count += 1
+
+                value = total / action_count # choosing uniformly at random from their legal moves
+                return value
+        
+        return expectimax(gameState, 0, 0)
 
 def betterEvaluationFunction(currentGameState: GameState):
     """
